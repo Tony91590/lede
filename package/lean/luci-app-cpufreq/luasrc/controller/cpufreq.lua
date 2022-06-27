08b1d80d4e54c29c4e7fd3081a311ab381c018aa
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2022-06-27 22:06:33 +0200

    Update cpufreq.lua

--- package/lean/luci-app-cpufreq/luasrc/controller/cpufreq.lua
+++ package/lean/luci-app-cpufreq/luasrc/controller/cpufreq.lua
@@ -1,12 +1,9 @@
-module("luci.controller.cpufreq",package.seeall)
+module("luci.controller.cpufreq", package.seeall)
 
 function index()
-	
-if not nixio.fs.access("/etc/config/cpufreq")then
-		
-return
-end
-	
-entry({"admin","services","cpufreq"},cbi("cpufreq"),_("CPU Freq"),900).dependent=false
-	
+	if not nixio.fs.access("/etc/config/cpufreq") then
+		return
+	end
+
+	entry({"admin", "services", "cpufreq"}, cbi("cpufreq"), _("CPU Freq"), 900).dependent = false
 end
