88de9357895609abfc3bc64124879011767b5440
xiangfeidexiaohuo <jsjson@163.com>
2022-09-29 23:18:14 +0800

    Revert "scripts/download.pl: fix mirrors regression for curl and wget"
    This reverts commit 5ae6c9be691778a7e4ccc6f429b92a98ea0e79a4.

--- scripts/download.pl
+++ scripts/download.pl
@@ -95,7 +95,6 @@ sub download_cmd {
 	}
 
 	if ($have_aria2c) {
-		@mirrors=();
 		return join(" ", "touch /dev/shm/${rfn}_spp;",
 			qw(aria2c --stderr -c -x2 -s10 -j10 -k1M), $url, $additional_mirrors,
 			$check_certificate ? () : '--check-certificate=false',
@@ -335,6 +334,7 @@ while (!-f "$target/$filename") {
 	if (!-f "$target/$filename" && $url_filename ne $filename) {
 		download($mirror, $filename, @mirrors);
 	}
+	@mirrors=();
 }
 
 $SIG{INT} = \&cleanup;
